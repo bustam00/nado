3fce3c6576d13d7fd0e0399e77db442751468720
DESKTOP-AAK1AJ1\bustam080 <bustam080@gmail.com>
2025-03-20 16:43:01 +0900

    star

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -11,12 +11,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 @Slf4j
 public class ArticleController {
     @Autowired
     private ArticleRepository articleRepository;
-
 
     @GetMapping("/articles/new")
     public String newArticleForm() {
@@ -33,7 +34,7 @@ public class ArticleController {
         Article saved = articleRepository.save(article);
         //System.out.println(saved.toString());
         log.info(saved.toString());
-        return "";
+        return "redirect:/articles/" + saved.getId();
     }
 
     @GetMapping("/articles/{id}")
@@ -51,6 +52,29 @@ public class ArticleController {
 
         //뷰 페이지 반환하기
         return "articles/show";
+    }
+    @GetMapping("/articles")
+    public String index(Model model){
+        //모든 데이터 가져오기
+        List<Article> articleEntityList = articleRepository.findAll();
+
+        //모델에 데티어 등록하기
+        model.addAttribute("articleList",articleEntityList);
+
+        //사용자에게 보여 줄 뷰 페이지 설정하기
+        return "articles/index";
+
+    }
+    @GetMapping("/articles/{id}/edit")
+    public String edit(@PathVariable Long id, Model model){
+        //수정할 데이터 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+
+        //모델에 데이터 등록하기
+        model.addAttribute("article",articleEntity);
+
+        //뷰 페이지 설정
+        return "articles/edit";
     }
 
 
